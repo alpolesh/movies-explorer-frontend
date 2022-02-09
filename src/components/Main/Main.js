614fec39e464402aa474e81bbc47ba7cd1d68b28
Andrey Poleshuck <alpoleshuck@gmail.com>
2022-02-09 22:10:39 +0300

    feat: add AboutProject and Techs components

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,13 @@
+import AboutProject from '../AboutProject/AboutProject';
 import Promo from '../Promo/Promo';
+import Techs from '../Techs/Techs';
 
 function Main() {
   return (
     <main className="main">
       <Promo />
+      <AboutProject />
+      <Techs />
       {/* <AboutProject />
       <Techs />
       <AboutMe />
