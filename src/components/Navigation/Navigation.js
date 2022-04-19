56e650330c910fbdb6a18483f5c81e624f4a27c6
Andrey Poleshuck <alpoleshuck@gmail.com>
2022-04-19 23:39:52 +0200

    feat: add burger-menu for tablet mode

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,21 +7,26 @@ function Navigation() {
     <nav className="header__navigation-container">
       {isMain.isExact ?
         (<div className="header__navigation">
-          <Link to="/register" style={{textDecoration: 'none'}}><p className="header__link header__link_route_main">Регистрация</p></Link>
-          <Link to="/login" style={{textDecoration: 'none'}}><button className="header__link-button header__link-button_route_main">Войти</button></Link>
+          <Link to="/register" className="header__link-container"><p className="header__link header__link_route_main">Регистрация</p></Link>
+          <Link to="/login" className="header__link-container"><button className="header__link-button header__link-button_route_main">Войти</button></Link>
         </div>) :
-        (<div className="header__navigation">
-          <Link to="/movies" style={{textDecoration: 'none'}}><p className="header__link header__link_type_bold">Фильмы</p></Link>
-          <Link to="/saved-movies" style={{textDecoration: 'none'}}><p className="header__link">Сохранённые фильмы</p></Link>
-          <Link to="/profile" style={{textDecoration: 'none'}}>
-            <div className="header__link-account-container">
-              <p className="header__link-account">Аккаунт</p>
-              <div className="header__link-icon-container">
-                <img className="header__link-icon" src={headerLinkIcon} alt="account icon" />
-              </div>
+        ( <>
+            <div className="header__burger-background"></div>
+            <div className="header__navigation">
+              <Link to="/movies" className="header__link-container"><p className="header__link header__link_type_bold">Фильмы</p></Link>
+              <Link to="/saved-movies" className="header__link-container"><p className="header__link">Сохранённые фильмы</p></Link>
+              <Link to="/profile" className="header__link-container">
+                <div className="header__link-account-container">
+                  <p className="header__link-account">Аккаунт</p>
+                  <div className="header__link-icon-container">
+                    <img className="header__link-icon" src={headerLinkIcon} alt="account icon" />
+                  </div>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>)}
+          </>
+        )
+      }
     </nav>
   )
 }
